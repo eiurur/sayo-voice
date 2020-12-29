04c0aacf6a3ceda4df43c152bb0ada5d871e1613
eiurur <eiurur789@gmail.com>
2020-12-29 22:19:13 +0900

    動画の切り出し対象を全キャラに変更、並列処理を追加して高速化

--- src/create_movie.py
+++ src/create_movie.py
@@ -3,9 +3,11 @@ import sys
 import cv2
 import os
 import shutil
-from tqdm.notebook import tqdm
+import traceback
+import joblib
+from tqdm import tqdm
 from datetime import datetime
-from multiprocessing import Pool
+from multiprocessing import current_process, Pool, Process
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 from pathlib import Path
 
@@ -13,7 +15,7 @@ from lib import fs
 from lib.image import Image
 
 CLIP_TARGET_FOLDER_NAMES = ["s1", "s2"]
-CHARACTOR_NAMES = ["sayo", "tsugumi", "tae", "kanon", "hina", "aya", "eve"]
+JOB_NUM = 3
 
 cwd = Path(os.path.dirname(os.path.abspath(__file__)))
 movie_dir = os.path.join(cwd.parent, 'movies')
@@ -33,44 +35,56 @@ def clip_movie(movie_file_path, clips, dst):
     final = concatenate_videoclips(clipsArray)
     final.write_videofile(dst, fps=video.fps, codec='libx264', audio_codec="aac")
 
+def process(src_record_path, chara_crop_dir, chara_tmp_dir):
+    try:
+        with open(src_record_path, encoding='UTF-8') as f:
+            lines = f.readlines()
+            movie_raw_file_path = lines[0].strip()
+            movie_encoded_file_name = lines[1].strip()
+
+            clips = list(map(lambda x: x.strip(), lines[2:]))
+            if len(clips) == 0: 
+                return
+
+            dst = os.path.join(chara_crop_dir, movie_encoded_file_name)
+            if os.path.exists(dst): 
+                return
+
+            tmp_file_path = os.path.join(chara_tmp_dir, movie_encoded_file_name)
+            shutil.copy2(movie_raw_file_path, tmp_file_path)
+            clip_movie(tmp_file_path, clips, dst)
+    except Exception as e:
+        print("process ERROR: ", src_record_path)
+        print(e)
+        print(traceback.format_exc())
+
+def prepare(charactor_name, series_name):
+    chara_crop_dir = os.path.join(crop_dir, charactor_name, series_name)
+    if not os.path.exists(chara_crop_dir):
+        os.makedirs(chara_crop_dir, exist_ok=True)
+
+    chara_tmp_dir = os.path.join(tmp_dir, charactor_name, series_name)
+    if not os.path.exists(chara_tmp_dir):
+        os.makedirs(chara_tmp_dir, exist_ok=True)
+
+    return chara_crop_dir, chara_tmp_dir
+    
 def main():
     for series_name in CLIP_TARGET_FOLDER_NAMES:
         charactor_dirs = fs.list_dirs(record_dir)
-        for charactor_name in charactor_dirs:
-            movies = fs.list_files(os.path.join(movie_dir, series_name))
+        for charactor_name in tqdm(charactor_dirs):
+            chara_crop_dir, chara_tmp_dir = prepare(charactor_name, series_name)
             chara_record_dir = os.path.join(record_dir, charactor_name, series_name)
-            records = fs.list_files(chara_record_dir)
+            record_pathes = fs.list_files(chara_record_dir)
+            joblib.Parallel(n_jobs=JOB_NUM)([joblib.delayed(process)(
+                src_record_path=record_path,
+                chara_crop_dir=chara_crop_dir,
+                chara_tmp_dir=chara_tmp_dir
+            ) for record_path in record_pathes])
 
-            chara_crop_dir = os.path.join(crop_dir, charactor_name, series_name)
-            if not os.path.exists(chara_crop_dir):
-                os.makedirs(chara_crop_dir, exist_ok=True)
-
-            chara_tmp_dir = os.path.join(tmp_dir, charactor_name, series_name)
-            if not os.path.exists(chara_tmp_dir):
-                os.makedirs(chara_tmp_dir, exist_ok=True)
-
-            pbar = tqdm(records)
-            for p in pbar:
-                with open(p, encoding='UTF-8') as f:
-                    lines = f.readlines()
-                    movie_raw_file_path = lines[0].strip()
-                    movie_encoded_file_name = lines[1].strip()
-
-                    clips = list(map(lambda x: x.strip(), lines[2:]))
-                    if len(clips) == 0: 
-                        pbar.update(1)
-                        continue
-
-                    dst = os.path.join(chara_crop_dir, movie_encoded_file_name)
-                    if os.path.exists(dst): 
-                        pbar.update(1)
-                        continue
-                    
-                    tmp_file_path = os.path.join(chara_tmp_dir, movie_encoded_file_name)
-                    shutil.copy2(movie_raw_file_path, tmp_file_path)
-                    clip_movie(tmp_file_path, clips, dst)
 
 if __name__ == "__main__":
     print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     main()
+    print('finished')
     
