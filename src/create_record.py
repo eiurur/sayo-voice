5887df4ed3ab2ecf95e9a2f5a7eec3f48b9367a7
eiurur <eiurur789@gmail.com>
2020-12-31 00:26:27 +0900

    記録間隔書き出し処理を並列処理化

--- src/create_record.py
+++ src/create_record.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import sys
 import os
-import math
-import glob
 import traceback
 import joblib
-import json
 from pathlib import Path
-from tqdm import tqdm
 from datetime import datetime
-from time import sleep
-from multiprocessing import current_process, Pool, Process
 
 from lib import fs
 from lib.image import Image
@@ -23,8 +16,8 @@ import tensorflow as tf
 from tensorflow import keras
 
 CLIP_TARGET_FOLDER_NAMES = ["s1", "s2"]
-JOB_NUM = 1
-THRESHOLD = 80.0
+JOB_NUM = 4
+THRESHOLD = 90.0
 IMAGE_SIZE_PX = 112
 SKIP_FRAME_INTEVAL = 4
 
@@ -35,14 +28,13 @@ class_mapping_file_path = os.path.join(cwd.parent, "model", "class_mapping.json"
 model_file_path = os.path.join(cwd.parent, "model", "dnn_model.h5")
 
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
-model = keras.models.load_model(model_file_path, compile=False)
 
 
 def process(src_movie_path, record_dir_format, class_mapping):
     try:
-        sleep(0.01)
         records = []
         for label in class_mapping.items():
+            model = keras.models.load_model(model_file_path, compile=False)
             record = Record()
             record.dir_format = record_dir_format
             record.label = label
@@ -58,7 +50,7 @@ def process(src_movie_path, record_dir_format, class_mapping):
         movie.capture()
         movie.write_period_to_file()
     except Exception as e:
-        print("SAMPLE_FUNC ERROR: ", src_movie_path)
+        print("record ERROR: ", src_movie_path)
         print(e)
         print(traceback.format_exc())
 
