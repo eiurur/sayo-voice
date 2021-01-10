20cd9d6bc8c97397c6cbce3179c12eaf2b0a7cef
eiurur <eiurur789@gmail.com>
2021-01-11 03:42:30 +0900

    例外処理追加

--- src/lib/fs.py
+++ src/lib/fs.py
@@ -1,19 +1,26 @@
 
 import os
 import json
+import time
 from hashlib import md5
 from time import localtime
 
 
 def list_entries(dir):
+    if not os.path.exists(dir):
+        return []
     return [os.path.join(dir, p.name) for p in os.scandir(dir)]
 
 
 def list_dirs(dir):
+    if not os.path.exists(dir):
+        return []
     return [p.name for p in os.scandir(dir) if os.path.isdir(os.path.join(dir, p.name))]
 
 
 def list_files(dir):
+    if not os.path.exists(dir):
+        return []
     return [p.name for p in os.scandir(dir) if os.path.isfile(os.path.join(dir, p.name))]
 
 
@@ -44,3 +51,12 @@ def load_json(file_path):
         json_data = json.load(json_file)
         return json_data
     return None
+
+
+def wait_until_generated(file_path, wait_sec=1, retry_count=10):
+    time_counter = 0
+    while not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
+        time.sleep(wait_sec)
+        time_counter += 1
+        if time_counter > retry_count:
+            break
